e43d64875e30fcb6077919ec4656a27bbc75303a
@emanuele.mele <emanuele.mele@keypartner.com>
2021-12-24 16:34:47 +0100

    Attributes updated

--- my-mysql/sql-scripts/CreateTable.sql
+++ my-mysql/sql-scripts/CreateTable.sql
@@ -3,12 +3,12 @@ id INT PRIMARY KEY,
 first_name  varchar(25) NOT NULL,
 second_name varchar(15) NOT NULL,
 email  varchar(50) NOT NULL,
-user_password varchar(256) NOT NULL,
+user_password varchar(64) NOT NULL,
 user_role varchar(50) NOT NULL,
-bio TEXT,
+bio varchar(128),
 photo_profile BLOB,
-date_reset TEXT,
-reset_code TEXT,
-p_iva TEXT,
-identity_card_number TEXT
+date_reset TIMESTAMP,
+reset_code varchar(64),
+p_iva varchar(11),
+identity_card_number varchar(7)
 );
