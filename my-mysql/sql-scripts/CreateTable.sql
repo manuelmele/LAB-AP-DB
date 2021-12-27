f0cd3ea242cb5d5273c0a016dfd69b0a38c2a07d
@emanuele.mele <emanuele.mele@keypartner.com>
2021-12-27 15:03:26 +0100

    Update Primary Id

--- my-mysql/sql-scripts/CreateTable.sql
+++ my-mysql/sql-scripts/CreateTable.sql
@@ -1,14 +1,16 @@
-CREATE TABLE accounts (
-id INT PRIMARY KEY,
-first_name  varchar(25) NOT NULL,
-second_name varchar(15) NOT NULL,
-email  varchar(50) NOT NULL,
-user_password varchar(64) NOT NULL,
-user_role varchar(50) NOT NULL,
-bio varchar(128),
-photo_profile BLOB,
-date_reset TIMESTAMP,
-reset_code varchar(64),
-p_iva varchar(11),
-identity_card_number varchar(7)
-);
+CREATE TABLE account (
+  id int NOT NULL AUTO_INCREMENT,
+  first_name varchar(64) NOT NULL,
+  second_name varchar(64) NOT NULL,
+  email varchar(64) NOT NULL,
+  user_password varchar(64) NOT NULL,
+  user_role varchar(8) DEFAULT NULL,
+  bio varchar(128) DEFAULT NULL,
+  photo_profile blob,
+  date_reset timestamp NULL DEFAULT NULL,
+  reset_code varchar(64) DEFAULT NULL,
+  p_iva varchar(11) DEFAULT NULL,
+  identity_card_number varchar(7) DEFAULT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY id_UNIQUE (id)
+) 
