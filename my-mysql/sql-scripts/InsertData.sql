646fd5d5a35f6ff190fa444382037d6752ca7520
@emanuele.mele <emanuele.mele@keypartner.com>
2022-03-01 21:10:46 +0100

    added real worker

--- my-mysql/sql-scripts/InsertData.sql
+++ my-mysql/sql-scripts/InsertData.sql
@@ -2,6 +2,4 @@
 INSERT INTO account (first_name, second_name, email, user_password, user_role, bio, category) 
 VALUES
 	('Marco', 'Rossi', 'marcorossi@live.it', '18239cc193556ead35daf750184084b2b210ac8a9130fa8d1c0f2440875c1317', 'Customer', 'Hi! My name is Marco.', 'Gardener'),
-	('Marcolino', 'Rossi', 'marcolinorossi@live.it', '18239cc193556ead35daf750184084b2b210ac8a9130fa8d1c0f2440875c1317', 'Worker', 'It ain''t much, but it''s honest work', 'Gardener' ),
-	('Marcopino', 'Rossi', 'marcopinorossi@live.it', '18239cc193556ead35daf750184084b2b210ac8a9130fa8d1c0f2440875c1317', 'Worker', 'It ain''t much, but it''s honest work', 'Gardener' ),
-    ('Marcotipo', 'Rossi', 'marcotiporossi@live.it', '18239cc193556ead35daf750184084b2b210ac8a9130fa8d1c0f2440875c1317', 'Worker', 'It ain''t much, but it''s honest work', 'Gardener' );
+	('Marta', 'Verdi', 'martaverdi@live.it', 'dc8d688bacca62d450640690fb92bc4045d7cebfae80966a454fac21b0b3418f', 'Worker', 'Idraulica specializzata in montaggio condizionatori e scaldabagni.', 'Plumber');
